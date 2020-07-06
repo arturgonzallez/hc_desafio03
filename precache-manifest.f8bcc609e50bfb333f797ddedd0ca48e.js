e14f3cac99c26a4d3148dab54189a7ee4e55e54d
Artur Gonzallez <arturgsb@gmail.com>
2020-07-06 19:59:34 -0300

    Criada primeira versão do projeto.

---
+++ precache-manifest.f8bcc609e50bfb333f797ddedd0ca48e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b922a8a436198fd2d49f494892294420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd3e2effa08ec8841ad",
+    "url": "/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "2784b2a9eff7b1189b89",
+    "url": "/static/css/main.c609114b.chunk.css"
+  },
+  {
+    "revision": "bfd3e2effa08ec8841ad",
+    "url": "/static/js/2.07bf35fe.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.07bf35fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2784b2a9eff7b1189b89",
+    "url": "/static/js/main.dddabe3d.chunk.js"
+  },
+  {
+    "revision": "e57b99add4aa28d82773",
+    "url": "/static/js/runtime-main.4598f4a2.js"
+  }
+]);
