d1e66f08b03410b7017f52e2c32060b5c131d4e6
Artur Barbosa <arturgsb@gmail.com>
2020-07-08 17:26:49 -0300

    Adicionado arquivos estáticos

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Desafio 03
+
+Protótipo de uma loja de Pokemon utilizando a api (https://pokeapi.co/) e ReactJs.
+
+Programa Hiring Coder - Gama Academy
---
+++ precache-manifest.3871cd8d5f9dd05be858213186b2760d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57c07adacadba3915a8511d86392bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9168d03e950035d91f02",
+    "url": "/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "fee908934f71f2d9f062",
+    "url": "/static/css/main.942569fd.chunk.css"
+  },
+  {
+    "revision": "9168d03e950035d91f02",
+    "url": "/static/js/2.771394c2.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.771394c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee908934f71f2d9f062",
+    "url": "/static/js/main.f73efd35.chunk.js"
+  },
+  {
+    "revision": "30601df86c79688506ee",
+    "url": "/static/js/runtime-main.d92af244.js"
+  }
+]);
